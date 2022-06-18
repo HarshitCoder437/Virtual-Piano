c006810f77dc2047a6e71c14c3042f4d05e7a7e7
Harshit <satyawaliharshit015@gmail.com>
2022-06-18 15:14:26 +0530

    Virtual Piano - The Free Online Piano

---
+++ index.js
@@ -0,0 +1,215 @@
+let sound1 = document.getElementById("note1");
+
+sound1.addEventListener("click", click1);
+
+function click1() {
+  let note1 = new Audio("sounds/key01.ogg");
+  note1.play();
+}
+
+let sound2 = document.getElementById("note2");
+
+sound2.addEventListener("click", click2);
+
+function click2() {
+  let note2 = new Audio("sounds/key02.ogg");
+  note2.play();
+}
+
+let sound3 = document.getElementById("note3");
+
+sound3.addEventListener("click", click3);
+
+function click3() {
+  let note3 = new Audio("sounds/key03.ogg");
+  note3.play();
+}
+
+let sound4 = document.getElementById("note4");
+
+sound4.addEventListener("click", click4);
+
+function click4() {
+  let note4 = new Audio("sounds/key04.ogg");
+  note4.play();
+}
+
+let sound5 = document.getElementById("note5");
+
+sound5.addEventListener("click", click5);
+
+function click5() {
+  let note5 = new Audio("sounds/key05.ogg");
+  note5.play();
+}
+
+let sound6 = document.getElementById("note6");
+
+sound6.addEventListener("click", click6);
+
+function click6() {
+  let note6 = new Audio("sounds/key06.ogg");
+  note6.play();
+}
+
+let sound7 = document.getElementById("note7");
+
+sound7.addEventListener("click", click7);
+
+function click7() {
+  let note7 = new Audio("sounds/key07.ogg");
+  note7.play();
+}
+
+let sound8 = document.getElementById("note8");
+
+sound8.addEventListener("click", click8);
+
+function click8() {
+  let note8 = new Audio("sounds/key08.ogg");
+  note8.play();
+}
+
+let sound9 = document.getElementById("note9");
+
+sound9.addEventListener("click", click9);
+
+function click9() {
+  let note9 = new Audio("sounds/key09.ogg");
+  note9.play();
+}
+
+let sound10 = document.getElementById("note10");
+
+sound10.addEventListener("click", click10);
+
+function click10() {
+  let note10 = new Audio("sounds/key10.ogg");
+  note10.play();
+}
+
+let sound11 = document.getElementById("note11");
+
+sound11.addEventListener("click", click11);
+
+function click11() {
+  let note11 = new Audio("sounds/key11.ogg");
+  note11.play();
+}
+
+let sound12 = document.getElementById("note12");
+
+sound12.addEventListener("click", click12);
+
+function click12() {
+  let note12 = new Audio("sounds/key12.ogg");
+  note12.play();
+}
+
+let sound13 = document.getElementById("note13");
+
+sound13.addEventListener("click", click13);
+
+function click13() {
+  let note13 = new Audio("sounds/key13.ogg");
+  note13.play();
+}
+
+let sound14 = document.getElementById("note14");
+
+sound14.addEventListener("click", click14);
+
+function click14() {
+  let note14 = new Audio("sounds/key14.ogg");
+  note14.play();
+}
+
+let sound15 = document.getElementById("note15");
+
+sound15.addEventListener("click", click15);
+
+function click15() {
+  let note15 = new Audio("sounds/key15.ogg");
+  note15.play();
+}
+
+let sound16 = document.getElementById("note16");
+
+sound16.addEventListener("click", click16);
+
+function click16() {
+  let note16 = new Audio("sounds/key16.ogg");
+  note16.play();
+}
+
+let sound17 = document.getElementById("note17");
+
+sound17.addEventListener("click", click17);
+
+function click17() {
+  let note17 = new Audio("sounds/key17.ogg");
+  note17.play();
+}
+
+let sound18 = document.getElementById("note18");
+
+sound18.addEventListener("click", click18);
+
+function click18() {
+  let note18 = new Audio("sounds/key18.ogg");
+  note18.play();
+}
+
+let sound19 = document.getElementById("note19");
+
+sound19.addEventListener("click", click19);
+
+function click19() {
+  let note19 = new Audio("sounds/key19.ogg");
+  note19.play();
+}
+
+let sound20 = document.getElementById("note20");
+
+sound20.addEventListener("click", click20);
+
+function click20() {
+  let note20 = new Audio("sounds/key20.ogg");
+  note20.play();
+}
+
+let sound21 = document.getElementById("note21");
+
+sound21.addEventListener("click", click21);
+
+function click21() {
+  let note21 = new Audio("sounds/key21.ogg");
+  note21.play();
+}
+
+let sound22 = document.getElementById("note22");
+
+sound22.addEventListener("click", click22);
+
+function click22() {
+  let note22 = new Audio("sounds/key22.ogg");
+  note22.play();
+}
+
+let sound23 = document.getElementById("note23");
+
+sound23.addEventListener("click", click23);
+
+function click23() {
+  let note23 = new Audio("sounds/key23.ogg");
+  note23.play();
+}
+
+let sound24 = document.getElementById("note24");
+
+sound24.addEventListener("click", click24);
+
+function click24() {
+  let note24 = new Audio("sounds/key24.ogg");
+  note24.play();
+}
